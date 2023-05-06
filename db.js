5452b86bb75914727f7d1159813a6727994684bc
Neerav-Khatri <khaneerav6337@gmail.com>
2023-05-06 15:00:54 +0530

    Backend

--- db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-const connection = mongoose.connect(`mongodb+srv://neeravkhatri:neeravkhatri@cluster0.dcucben.mongodb.net/Masai_Library?retryWrites=true&w=majority`);
-
-module.exports = { connection };
